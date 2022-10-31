924a550a3c2ebd1c5e03cc490b21ff8db9c4b08e
Eren <vrsozluk@gmail.com>
2022-10-31 17:23:15 +0300

    JS has been added

---
+++ main.js
@@ -0,0 +1,17 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+        item.onclick = () => {
+          if (item.id == "clear") {
+            display.innerText = "";
+          } else if (display.innerText != "" && item.id == "equal") {
+            display.innerText = eval(display.innerText);
+          } else if (display.innerText == "" && item.id == "equal") {
+            display.innerText = "Empty!";
+            setTimeout(() => (display.innerText = ""), 2000);
+          } else {
+            display.innerText += item.id;
+          }
+        };
+      });
